7acf1492fd89bb875fd7ae0c548517070cfdfdc5
DanielJames <james.daniel.isaiah@gmail.com>
2020-04-11 12:33:21 -0400

    no message

---
+++ RES/ReuasableFunctions/Sizes.swift
@@ -0,0 +1,26 @@
+//
+//  VariadicFunction.swift
+//  RES
+//
+//  Created by Daniel James on 4/9/20.
+//  Copyright © 2020 CUIP. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController{
+    
+    var ScreenSize: CGRect {
+        return UIScreen.main.bounds
+    }
+    
+}
+
+extension UIView{
+    
+    var ScreenSize: CGRect {
+        return UIScreen.main.bounds
+    }
+    
+}
